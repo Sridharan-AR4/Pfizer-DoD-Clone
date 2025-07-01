a23dae3c3660d66d5617a9f4dc29d10e676a5ceb
Sridharan-AR4 <sridhar47914@gmail.com>
2025-07-01 10:07:53 +0530

    language popup added

--- scripts/header.js
+++ scripts/header.js
@@ -1,5 +1,5 @@
 function displayLoginPopUp(){
-   const lockcontainer = document.getElementById("login-pop-up")
+   const lockcontainer = document.getElementById("login-pop-up");
 
    const computedStyle = window.getComputedStyle(lockcontainer);
 
@@ -10,4 +10,14 @@ function displayLoginPopUp(){
     lockcontainer.style.display = 'none';
    }
    
+}
+function displayOrHideLangPopUP(){
+   const langcontainer = document.getElementById("choose-lang-container");
+   const computedStyle = window.getComputedStyle(langcontainer);
+   if(computedStyle['display'] == 'none'){
+      langcontainer.style.display = 'flex';
+   }
+   else{
+      langcontainer.style.display = 'none';
+   }
 }
